35b3642cadcb61736b3a16208ddc68bbbfb44209
Achim Winter <achim.winter95@gmail.com>
2022-06-08 23:34:31 +0200

    implement filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'
 import './ExpenseItem.css'
@@ -9,7 +7,7 @@ const ExpenseItem = props => {
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}$</div>
             </div>
             <button>Change Title</button>
