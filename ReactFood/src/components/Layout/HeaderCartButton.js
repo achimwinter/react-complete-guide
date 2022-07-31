7ba755234e0d584801e7b02ea8b0a0c4accdb1cc
Achim Winter <achim.winter95@gmail.com>
2022-07-31 23:58:19 +0200

    useReducer implemented

--- ReactFood/src/components/Layout/HeaderCartButton.js
+++ ReactFood/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,25 @@
+import { useContext } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = props => {
-    return <button className={classes.button}>
+    const cartCtx = useContext(CartContext);
+
+    const {items} = cartCtx;
+
+    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+        return currentNumber + item.amount;
+    }, 0)
+
+    return <button className={classes.button} onClick={props.onClick}>
         <span>
             <CartIcon className={classes.icon}/>
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>
-            3
+            {numberOfCartItems}
         </span>
     </button>
 }
